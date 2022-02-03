9e4859b8974e866ed3ab7588e6c8b49bd08c7d72
Mehmet CEBESOY <mhmtcbsy@gmail.com>
2022-02-03 21:50:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+	// kod satırının bitttiğini ; ile  anlarız
+        System.
+                out.
+                println
+                        ("Merhaba Java");
+
+    }
+}
